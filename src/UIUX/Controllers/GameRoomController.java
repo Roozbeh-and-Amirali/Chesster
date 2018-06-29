e7a4f5c233dc9fbf52482b6c58955a6512b21cf7
monjar <amirali.mnj@gmail.com>
2018-06-29 10:12:44 -0700

    Chat bugs FIxed 8 e tir.

--- src/UIUX/Controllers/GameRoomController.java
+++ src/UIUX/Controllers/GameRoomController.java
@@ -121,7 +121,6 @@ public class GameRoomController extends ParentController implements Initializabl
 
 
         Thread waiter = new Thread(waitForPlayers());
-
         waiter.start();
 
         Thread chatReciever = new Thread(waitForChat());
